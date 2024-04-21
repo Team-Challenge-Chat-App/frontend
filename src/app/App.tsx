4f41140989453b1bf0e76c2155ee6dc344b02130
Nazarii <hoolyworkacc@gmail.com>
2024-04-21 22:52:27 +0300

    chore: reformat with another tabwidth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-    return <h1 className="p-4 pt-2 text-center text-2xl">Hello World</h1>;
+  return <h1 className="p-4 pt-2 text-center text-2xl">Hello World</h1>;
 }
 
 export default App;
