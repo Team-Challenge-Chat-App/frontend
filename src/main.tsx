85ad8bae1e351b4ebd5d156f1cce3e31c9bde091
Nazarii <hoolyworkacc@gmail.com>
2024-04-21 22:50:43 +0300

    chore: rename component

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { RouterProvider } from '@/lib/router/router-provider.tsx';
+import { RouterProvider } from '@/lib/router/RouterProvider.tsx';
 import { store } from '@/lib/store/store.ts';
 
 import App from './app/App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider routes={[{ path: '/', element: <App /> }]} />
-        </Provider>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider routes={[{ path: '/', element: <App /> }]} />
+    </Provider>
+  </React.StrictMode>,
 );
