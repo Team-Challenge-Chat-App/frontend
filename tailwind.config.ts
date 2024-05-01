3cd798772331b5ac190371d92d148f6e88d147a3
HoolyDuck <nazar260804@gmail.com>
2024-05-02 01:08:02 +0300

    feat: setup tailwind classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        btnPrimary: 'var(--button-primary)',
+        btnPrimaryHover: 'var(--button-primary-hover)',
+        btnPrimaryActive: 'var(--button-primary-active)',
+
+        btnSecondary: 'var(--button-secondary)',
+        btnSecondaryHover: 'var(--button-secondary-hover)',
+        btnSecondaryActive: 'var(--button-secondary-active)',
+      },
+    },
   },
   plugins: [],
 };
