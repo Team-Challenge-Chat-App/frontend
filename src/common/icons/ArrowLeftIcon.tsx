60022f6cbafad9de0137fb604c695988cf2ce80b
HoolyDuck <nazar260804@gmail.com>
2024-05-06 17:54:42 +0300

    feat: add icons

---
+++ src/common/icons/ArrowLeftIcon.tsx
@@ -0,0 +1,21 @@
+function ArrowLeftIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.3335 4.66699L19.3335 11.667M19.3335 11.667L12.3335 18.667M19.3335 11.667L5.3335 11.667"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export { ArrowLeftIcon };
